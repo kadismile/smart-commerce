f77b9cb3a9f43f00bf7647c44da1f0284ddb2435
Orkhai Dickson <dicksonorkhai@gmail.com>
2022-04-19 10:50:25 +0100

    Added Cart components

---
+++ src/components/Cart/CartEject.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CartEject = () => {
+  return (
+    <a
+      className="ps-product__remove"
+      // href="javascript: void(0)"
+      href="#/"
+    >
+      <i className="icon-cross" />
+    </a>
+  );
+};
+
+export default CartEject;
