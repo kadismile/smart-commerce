aaac73e87c29a5c67f92cd14184667512a77e33d
Orkhai Dickson <dicksonorkhai@gmail.com>
2022-04-24 16:13:45 +0100

    Fixed mobile cart page into table and added 404 page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -92,6 +92,108 @@ const Cart = () => {
                       </a>
                     </td>
                   </tr>
+                  <tr>
+                    <td colSpan={2} className="align-middle text-center">
+                      <a
+                        href="product1.html"
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        {/* <figure style={{ height: "3rem", width: "3rem" }}> */}
+                        <img
+                          style={{
+                            height: "3rem",
+                            width: "3rem",
+                            marginRight: "1rem",
+                          }}
+                          src="img/products/001.jpg"
+                          alt=""
+                        />
+                        Digital Thermometer X30-Pro
+                        {/* </figure> */}
+                      </a>
+                    </td>
+                    <td className="align-middle text-center">
+                      {" "}
+                      <span>$77.65</span>
+                    </td>
+                    <td className="align-middle text-center">
+                      <div>
+                        <input
+                          style={{ width: "2rem" }}
+                          min={0}
+                          name="quantity"
+                          defaultValue={1}
+                          type="number"
+                        />
+                      </div>
+                    </td>
+                    <td
+                      style={{ color: "#fd8d27" }}
+                      className="align-middle text-center"
+                    >
+                      $77.65
+                    </td>
+                    <td className="align-middle text-center">
+                      {" "}
+                      <a href="#/">
+                        <i className="icon-cross" />
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td colSpan={2} className="align-middle text-center">
+                      <a
+                        href="product1.html"
+                        style={{
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        {/* <figure style={{ height: "3rem", width: "3rem" }}> */}
+                        <img
+                          style={{
+                            height: "3rem",
+                            width: "3rem",
+                            marginRight: "1rem",
+                          }}
+                          src="img/products/001.jpg"
+                          alt=""
+                        />
+                        Digital Thermometer X30-Pro
+                        {/* </figure> */}
+                      </a>
+                    </td>
+                    <td className="align-middle text-center">
+                      {" "}
+                      <span>$77.65</span>
+                    </td>
+                    <td className="align-middle text-center">
+                      <div>
+                        <input
+                          style={{ width: "2rem" }}
+                          min={0}
+                          name="quantity"
+                          defaultValue={1}
+                          type="number"
+                        />
+                      </div>
+                    </td>
+                    <td
+                      style={{ color: "#fd8d27" }}
+                      className="align-middle text-center"
+                    >
+                      $77.65
+                    </td>
+                    <td className="align-middle text-center">
+                      {" "}
+                      <a href="#/">
+                        <i className="icon-cross" />
+                      </a>
+                    </td>
+                  </tr>
                 </tbody>
               </table>
               {/* <ul className="ps-shopping__list">
@@ -200,6 +302,118 @@ const Cart = () => {
                     </tr>
                   </thead>
                   <tbody>
+                    <tr>
+                      <td className="ps-product__remove">
+                        {" "}
+                        <a href="#/">
+                          <i className="icon-cross" />
+                        </a>
+                      </td>
+                      <td className="ps-product__thumbnail">
+                        <a className="ps-product__image" href="product1.html">
+                          <figure>
+                            <img src="img/products/001.jpg" alt="" />
+                          </figure>
+                        </a>
+                      </td>
+                      <td className="ps-product__name">
+                        {" "}
+                        <a href="product1.html">Digital Thermometer X30-Pro</a>
+                      </td>
+                      <td className="ps-product__meta">
+                        {" "}
+                        <span className="ps-product__price sale">$77.65</span>
+                        <span className="ps-product__del">$80.65</span>
+                      </td>
+                      <td className="ps-product__quantity">
+                        <div className="def-number-input number-input safari_only">
+                          <button
+                            className="minus"
+                            onClick={() =>
+                              this.parentNode
+                                .querySelector("input[type=number]")
+                                .stepDown()
+                            }
+                          >
+                            <i className="icon-minus" />
+                          </button>
+                          <input
+                            className="quantity"
+                            min={0}
+                            name="quantity"
+                            defaultValue={1}
+                            type="number"
+                          />
+                          <button
+                            className="plus"
+                            onClick={() =>
+                              this.parentNode
+                                .querySelector("input[type=number]")
+                                .stepUp()
+                            }
+                          >
+                            <i className="icon-plus" />
+                          </button>
+                        </div>
+                      </td>
+                      <td className="ps-product__subtotal">$77.65</td>
+                    </tr>
+                    <tr>
+                      <td className="ps-product__remove">
+                        {" "}
+                        <a href="#/">
+                          <i className="icon-cross" />
+                        </a>
+                      </td>
+                      <td className="ps-product__thumbnail">
+                        <a className="ps-product__image" href="product1.html">
+                          <figure>
+                            <img src="img/products/001.jpg" alt="" />
+                          </figure>
+                        </a>
+                      </td>
+                      <td className="ps-product__name">
+                        {" "}
+                        <a href="product1.html">Digital Thermometer X30-Pro</a>
+                      </td>
+                      <td className="ps-product__meta">
+                        {" "}
+                        <span className="ps-product__price sale">$77.65</span>
+                        <span className="ps-product__del">$80.65</span>
+                      </td>
+                      <td className="ps-product__quantity">
+                        <div className="def-number-input number-input safari_only">
+                          <button
+                            className="minus"
+                            onClick={() =>
+                              this.parentNode
+                                .querySelector("input[type=number]")
+                                .stepDown()
+                            }
+                          >
+                            <i className="icon-minus" />
+                          </button>
+                          <input
+                            className="quantity"
+                            min={0}
+                            name="quantity"
+                            defaultValue={1}
+                            type="number"
+                          />
+                          <button
+                            className="plus"
+                            onClick={() =>
+                              this.parentNode
+                                .querySelector("input[type=number]")
+                                .stepUp()
+                            }
+                          >
+                            <i className="icon-plus" />
+                          </button>
+                        </div>
+                      </td>
+                      <td className="ps-product__subtotal">$77.65</td>
+                    </tr>
                     <tr>
                       <td className="ps-product__remove">
                         {" "}
