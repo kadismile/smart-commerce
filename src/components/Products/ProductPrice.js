6f85779009771c7e40b146e1ec5ef28de5018fa7
Orkhai Dickson <dicksonorkhai@gmail.com>
2022-04-24 15:42:06 +0100

    Fixed mobile cart page into table and added 404 page

--- src/components/Products/ProductPrice.js
+++ src/components/Products/ProductPrice.js
@@ -3,7 +3,7 @@ import React from "react";
 const ProductPrice = () => {
   return (
     <div className="ps-product__meta">
-      <span className="ps-product__sale">$38.39</span>
+      <span className="ps-product__price sale">$38.39</span>
       <span className="ps-product__del">$35.99</span>
     </div>
   );
